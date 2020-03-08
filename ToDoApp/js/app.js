dcfa67c112bf966560e1b64ee5557c5fa10c437b
r0nin <georgi.pronenkov@gmail.com>
2020-03-08 21:31:55 +0200

    initial commit

--- ToDoApp/js/app.js
+++ ToDoApp/js/app.js
@@ -9,5 +9,75 @@ const CHECK = "fa-check-circle";
 const UNCHECK = "fa-circle-thin";
 const LINE_THROUGH = "lineThrough";
 
-//variables///////////////////////////
+//variables
+let LIST = [],
+      id = 0;
 
+//show today date:
+const today = new Date();
+const options = { weekday:"long", month:"short", day:"numeric" };
+dateElement.innerHTML = today.toLocaleDateString("en-US", options);
+
+//add to-do method
+function addToDo(toDo, id, trash) {
+
+    if (trash) {
+        return;
+    }
+
+    const DONE = done ? CHECK : UNCHECK;
+    const LINE = done ? LINE_THROUGH : "";
+
+    const item = `
+                   <li class="item">
+                     <i class="fa ${DONE} co" job="complete" id="${id}"></i>
+                    <p class="text ${LINE}">${toDo}</p>
+                    <i class="fa fa-trash-o de" job="delete" id="${id}"></i>
+                   </li> 
+                 `;
+    const position = "beforeend";
+    list.insertAdjacentHTML(position, item);
+}
+
+//add item to the list user
+document.addEventListener("keyup", function (e) {
+    if (event.keyCode === 13) { //13 is enter key
+        const toDo = input.value;
+        if (toDo) {
+            //if input is not empty:
+            addToDo(toDo, id, false, false);
+
+            LIST.push({
+               name: toDo,
+               id: id,
+               done: false,
+               trash: false
+            });
+
+            id++;
+        }
+        input.value = '';
+    }
+});
+
+//complete to-do
+function completeToDo(element) {
+    element.classList.toggle(CHECK);
+    element.classList.toggle(UNCHECK);
+    element.parentNode
+            .querySelector(".text")
+            .classList
+            .toggle(LINE_THROUGH);
+
+    //update list
+    LIST[element.id].done = !LIST[element.id].done;
+}
+
+//remove to-do
+function removeTodo(element) { //element - item from user interface
+    element.parentNode.parentNode
+                      .removeChild(element.parentNode);
+    LIST[element.id].trash = true;
+}
+
+//target items created dynamically
