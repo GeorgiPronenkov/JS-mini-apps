337e8c35848efa244998176a9b8d94f0f9e774e5
r0nin <georgi.pronenkov@gmail.com>
2020-03-08 17:24:50 +0200

    qqq

---
+++ ToDoApp/js/app.js
@@ -0,0 +1,13 @@
+//select elements:
+const clear = document.querySelector(".clear");
+const dateElement = document.getElementById("date");
+const list = document.getElementById("list");
+const input = document.getElementById("input");
+
+//classes names
+const CHECK = "fa-check-circle";
+const UNCHECK = "fa-circle-thin";
+const LINE_THROUGH = "lineThrough";
+
+//variables
+
