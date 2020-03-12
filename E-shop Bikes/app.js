d27df7e208207727a5ecd4c2a97cde181d2ce4cd
r0nin <georgi.pronenkov@gmail.com>
2020-03-12 18:57:43 +0200

    init

---
+++ E-shop Bikes/app.js
@@ -0,0 +1,34 @@
+//set variables:
+const cartBtn = document.querySelector('.cart-btn');
+const closeCartBtn = document.querySelector('.close-cart');
+const clearCartbtn = document.querySelector('.clear-cart');
+const cartDOM = document.querySelector('.cart');
+const cartOverlay = document.querySelector('.cart-overlay');
+const cartItems = document.querySelector('.cart-items');
+const cartTotal = document.querySelector('.cart-total');
+const cartContent = document.querySelector('.cart-content');
+const productsDOM = document.querySelector('.products-center');
+const productsDOM = document.querySelector('.products-center');
+
+//cart
+let cart = [];
+
+//class for getting the products
+class Products {
+
+}
+
+//UI  class - display products
+class UI {
+
+}
+
+//local storage class
+class Storage {
+
+}
+
+//eventListener for click
+document.addEventListener("DOMContentLoaded", () => {
+    
+})
