6148d58804954d83a0efd82d880b78531c1c9ef0
r0nin <georgi.pronenkov@gmail.com>
2020-03-17 04:53:15 +0200

    init

---
+++ Weight Converter/app.js
@@ -0,0 +1,16 @@
+document.getElementById('output').style.visibility = 'hidden';
+let inputUnit = document.getElementById('kgInput');
+
+inputUnit.addEventListener('input', function (e) {
+            document.getElementById('output').style.visibility = 'visible';
+
+            let kgs = e.target.value;
+            document.getElementById('gramsOutput')
+                    .innerHTML = kgs*1000;
+            document.getElementById('lbsOutput')
+                    .innerHTML = kgs * 2.2046226218;
+            document.getElementById('ozOutput')
+                    .innerHTML = kgs * 35.27396195;
+        });
+
+inputUnit.value = '';
