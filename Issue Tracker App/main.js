233883463da534583d410b658d9aa6830a4cf472
Georgi Pronenkov <45390436+Pronenkov@users.noreply.github.com>
2020-03-19 02:17:03 +0200

    init

---
+++ Issue Tracker App/main.js
@@ -0,0 +1,25 @@
+function fetchIssues() {
+    const issues = JSON.parse(localStorage.getItem(íssues));
+    const issuesList = document.getElementById('issuesList');
+
+    issuesList.innerHTML = '';
+
+    for (let index = 0; index < issues.length; index++) {
+        const id = issues[index].id;
+        const desc = issues[index].description;
+        const severity = issues[index].severity;
+        const assignedTo = issues[index].assignedTo;
+        const status = issues[index].status;
+
+        issuesList.innerHTML += '<div class="well">' + 
+                                '<h6>Issue ID: ' + id + '</h6>' + 
+                                '<p><span class="label label-info">' + status + '</span></p>' +
+                                '<h3>' + desc + '</h3>' +
+                                '<p><span class="glyphicon glyphicon-time"></span>' + severity + '</p>' +
+                                '<p><span class="glyphicon glyphicon-user"></span>' + assignedTo + '</p>' +
+                                '<a href="#" onclick="setStatusClosed(\''+id+'\')" class="btn btn-warning">Close</a>' + 
+                                '<a href="#" onclick="deleteIsssue(\''+id+'\')" class="btn btn-danger">Delete</a>' +
+                                '</div>';
+        
+    }
+}
