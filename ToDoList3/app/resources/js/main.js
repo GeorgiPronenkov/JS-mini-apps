128fc6273ab7a4574894188d2860685680464e7e
Georgi Pronenkov <45390436+Pronenkov@users.noreply.github.com>
2020-03-20 01:47:57 +0200

    init

---
+++ ToDoList3/app/resources/js/main.js
@@ -0,0 +1,9 @@
+//user click on the add button
+document.getElementById('add').addEventListener('click', function () {
+   const people = document.getElementById('item').value;
+   if (value) {
+       
+   } else {
+       
+   }
+});
