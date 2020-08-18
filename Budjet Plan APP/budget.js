a53de7b8124dfe8695a26e32ac26515b55987175
Georgi <georgi.pronenkov@gmail.com>
2020-08-18 13:21:33 +0300

    init

---
+++ Budjet Plan APP/budget.js
@@ -0,0 +1,24 @@
+//select elements
+const balanceElement = document.querySelector(".balance .value");
+const incomeTotalElement = document.querySelector(".income-total");
+const outcomeTotalElement = document.querySelector(".outcome-total");
+const incomeElement = document.querySelector("#income");
+const expenseElement = document.querySelector("#expense");
+const allElement = document.querySelector("#all");
+const incomeList = document.querySelector("#income .list");
+const expenceList = document.querySelector("#outcome .list");
+const allList = document.querySelector("#all .list");
+
+//select the buttons
+const expenseBtn = document.querySelector(".tab1");
+const incomeBtn = document.querySelector(".tab2");
+const allBtn = document.querySelector(".tab3");
+
+//select input btns
+const addExpense = document.querySelector(".add-expense");
+const expenseTitle = document.getElementById("expense-title-input");
+const expenseAmount = document.getElementById("expense-amount-input");
+
+const addIncome = document.querySelector(".add-income");
+const incomeTitle = document.getElementById("income-title-input");
+const incomeAmount = document.getElementById("income-amount-input");
